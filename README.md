fea7f71f46a69c7f365f5fe367945b7318860e00
Mikhail Kuperman <kuperman.mikhail@gmail.com>
2017-04-07 12:04:28 +0300

    Add NN examples

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# analyze
+
+### Setup on Windows x64
+
+Anaconda 4.2 (Python 3.5)
+
+Packages:
+
+1. scikit-learn  
+2. theano  
+'conda install mingw libpython theano=0.8'
+3. tensorflow  
+'pip install --ignore-installed --upgrade https://storage.googleapis.com/tensorflow/windows/gpu/tensorflow_gpu-1.0.1-cp35-cp35m-win_amd64.whl'
+3. keras  
+'conda install -c conda-forge keras'
+4. lasagne  
+'pip install https://github.com/Lasagne/Lasagne/archive/master.zip'
+5. nolearn  
+'pip install nolearn'
+6. seaborn  
+'conda install seaborn'
+
+### Examples
+
+learn.py:
+
+1. Logistic regression
+2. kNN
+3. SVM
+4. Random forest
+5. Gaussian Process (don't run on > 5000 samples!)
+6. Multi-layer Perceptron (scikit built-in Neural Network) - 2 solvers
+7. Keras
+8. Lasagne
+
+Use flag variables to switch models on/off
+
+Use Env variables on top to tune NN backends
+
+Settings for all NN examples are similar so result are very close
--- learn.py
+++ learn.py
@@ -1,34 +1,51 @@
+import os
+# Keras backend settings
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"] = "5" # or coment it to use GPU by default
+os.environ["KERAS_BACKEND"] = "theano" # or coment it to use tensorflow by default
+
+# Theano settings
+os.environ["THEANO_FLAGS"] = "device=cpu" # or "device=cuda"
+
 from sklearn import preprocessing
 from sklearn.cross_validation import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.grid_search import GridSearchCV
 from sklearn.svm import SVC
 from sklearn import ensemble
-from  sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 from sklearn import metrics
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
+from sklearn.neural_network import MLPClassifier
+
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras import regularizers
+
+from lasagne.layers import DenseLayer
+from lasagne.layers import InputLayer
+from lasagne import nonlinearities, updates, objectives
+from lasagne.layers import get_all_params
+
+from nolearn.lasagne import NeuralNet
+from nolearn.lasagne import TrainSplit
+from nolearn.lasagne import objective
 
 import matplotlib.pyplot as plt
-import seaborn as sns
 import numpy as np
 import pandas as pd
 
-def PrintTest(classifier):
-    y_pred = classifier.predict(X_test)
-    if hasattr(classifier, "predict_proba"):
-        proba = classifier.predict_proba(X_test)
-        y_proba = proba[:,1]
-        print ('log_loss: ', metrics.log_loss(y_test, proba))
-    else:  # use decision function
-        prob_pos = classifier.decision_function(X_test)
-        y_proba = (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())    
-        
-    print ('brier_score_loss: ', metrics.brier_score_loss(y_test, y_proba))    
-    print(metrics.classification_report(y_test, y_pred))
+KNN = 0
+SV = 0
+RF = 1
+GP = 0
+MLP = 1
+KERAS = 1
+LASAGNE = 1
 
-sns.set(color_codes=True)
-data = pd.read_csv('drivers_5000.csv')
+data = pd.read_csv('drivers_250000.csv')
 
 data.at[data['Accidents'] > 0, 'AccidentsBin'] = 1
 data.at[data['Accidents'] == 0, 'AccidentsBin'] = 0
@@ -51,48 +68,119 @@ X = preprocessing.scale(XX)
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.5, random_state = 11)
 
-print ("kNN")
-#f_neighbors_array = [1, 3, 5, 7, 10, 15, 25]
-#knn = KNeighborsClassifier()
-#grid = GridSearchCV(knn, param_grid={'n_neighbors': f_neighbors_array})
-#grid.fit(X_train, y_train)
-#
-#best_cv_err = 1 - grid.best_score_
-#best_n_neighbors = grid.best_estimator_.n_neighbors
-#print (best_cv_err, best_n_neighbors)
-#
-#knn = KNeighborsClassifier(n_neighbors=best_n_neighbors)
-knn = KNeighborsClassifier(25)
-knn.fit(X_train, y_train)
-PrintTest(knn)
-
-print ("SVC")
-#C_array = np.logspace(-2, 2, num=3)
-#gamma_array = np.logspace(-3, 2, num=3)
-svc = SVC(kernel='rbf')
-#grid = GridSearchCV(svc, param_grid={'C': C_array, 'gamma': gamma_array})
-#grid.fit(X_train, y_train)
-#print ('CV error    = ', 1 - grid.best_score_)
-#print ('best C      = ', grid.best_estimator_.C)
-#print ('best gamma  = ', grid.best_estimator_.gamma)
-
-#svc = SVC(kernel='rbf', C=grid.best_estimator_.C, gamma=grid.best_estimator_.gamma)
-svc.fit(X_train, y_train)
-PrintTest(svc)
-
-print ("Forest")
-rf = ensemble.RandomForestClassifier(n_estimators=100, random_state=11)
-rf.fit(X_train, y_train)
-PrintTest(rf)
+def PrintTest(classifier, X_test=X_test, y_test=y_test):
+    y_pred = classifier.predict(X_test)
+    if hasattr(classifier, "predict_proba"):
+        proba = classifier.predict_proba(X_test)
+        if proba.shape[1] == 2:
+            y_proba = proba[:,1]
+        else:
+            y_proba = proba
+        print ('log_loss: ', metrics.log_loss(y_test, proba))
+    else:  # use decision function
+        prob_pos = classifier.decision_function(X_test)
+        y_proba = (prob_pos - prob_pos.min()) / (prob_pos.max() - prob_pos.min())    
+        
+    print ('brier_score_loss: ', metrics.brier_score_loss(y_test, y_proba))    
+    print(metrics.classification_report(y_test, y_pred))
 
 print ("Logistic Regression")
 lr = LogisticRegression()
 lr.fit(X_train, y_train)
 PrintTest(lr)
 
-print ("Gaussian Process Classifier")
-kernel = 1.0 * RBF([1.0])
-gpc_rbf_isotropic = GaussianProcessClassifier(kernel=kernel)
-gpc_rbf_isotropic.fit(X_train, y_train)
-PrintTest(gpc_rbf_isotropic)
+if KNN == 1:
+    print ("kNN")
+    #f_neighbors_array = [1, 3, 5, 7, 10, 15, 25]
+    #knn = KNeighborsClassifier()
+    #grid = GridSearchCV(knn, param_grid={'n_neighbors': f_neighbors_array})
+    #grid.fit(X_train, y_train)
+    #
+    #best_cv_err = 1 - grid.best_score_
+    #best_n_neighbors = grid.best_estimator_.n_neighbors
+    #print (best_cv_err, best_n_neighbors)
+    #
+    #knn = KNeighborsClassifier(n_neighbors=best_n_neighbors)
+    knn = KNeighborsClassifier(25)
+    knn.fit(X_train, y_train)
+    PrintTest(knn)
 
+if SV == 1:
+    print ("SVC")
+    #C_array = np.logspace(-2, 2, num=3)
+    #gamma_array = np.logspace(-3, 2, num=3)
+    svc = SVC(kernel='rbf', random_state = 11)
+    #grid = GridSearchCV(svc, param_grid={'C': C_array, 'gamma': gamma_array})
+    #grid.fit(X_train, y_train)
+    #print ('CV error    = ', 1 - grid.best_score_)
+    #print ('best C      = ', grid.best_estimator_.C)
+    #print ('best gamma  = ', grid.best_estimator_.gamma)
+    
+    #svc = SVC(kernel='rbf', C=grid.best_estimator_.C, gamma=grid.best_estimator_.gamma)
+    svc.fit(X_train, y_train)
+    PrintTest(svc)
+
+if RF == 1:
+    print ("Forest")
+    rf = ensemble.RandomForestClassifier(n_estimators=500, n_jobs = -1, random_state=11, verbose=1)
+    rf.fit(X_train, y_train)
+    PrintTest(rf)
+    
+if GP == 1:
+    print ("Gaussian Process Classifier")
+    kernel = 1.0 * RBF([1.0])
+    gpc_rbf_isotropic = GaussianProcessClassifier(kernel=kernel)
+    gpc_rbf_isotropic.fit(X_train, y_train)
+    PrintTest(gpc_rbf_isotropic)
+
+if MLP == 1:
+    print ("MLP (adam)")
+    mlp = MLPClassifier(solver='adam', alpha=1e-5, hidden_layer_sizes=(12, 6), random_state=11, verbose=1)
+    mlp.fit(X_train, y_train)
+    PrintTest(mlp)
+
+    print ("MLP (lbfgs)")
+    mlp = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(12, 6), random_state=11, verbose=1)
+    mlp.fit(X_train, y_train)
+    PrintTest(mlp)
+
+if KERAS == 1:
+    print ("Keras")
+    def CreateModel():
+    	model = Sequential()
+    	model.add(Dense(12, input_dim=X.shape[1], activation='relu'))
+    	model.add(Dense(6, activation='relu'))
+    	model.add(Dense(1, activation='sigmoid', kernel_regularizer=regularizers.l2(0.001), activity_regularizer=regularizers.l1(0.001)))
+    	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['mae'])
+    	return model
+    
+    model = KerasClassifier(build_fn=CreateModel, epochs=50, batch_size=100, verbose=1)
+    model.fit(X_train.astype(np.float32), y_train.values.astype(np.int32))
+    print("\n")
+    PrintTest(model, X_test.astype(np.float32), y_test.values.astype(np.int32))
+
+if LASAGNE == 1:
+    print ("Lasagne")
+    layers0 = [
+        (InputLayer, {'shape': (None, X.shape[1])}),
+        (DenseLayer, {'num_units': 12}),
+        (DenseLayer, {'num_units': 6}),
+        (DenseLayer, {'num_units': 1, 'nonlinearity': nonlinearities.sigmoid}),
+    ]
+    
+    ls = NeuralNet(
+        layers=layers0,
+        max_epochs=50,
+    
+        update=updates.adam,
+    
+        objective_l2=0.001,
+        objective_loss_function=objectives.binary_crossentropy,
+    
+        train_split=TrainSplit(eval_size=0.25),
+        verbose=1,
+    )
+    
+    ls.fit(X_train.astype(np.float32), y_train.values.astype(np.int32))
+    print("\n")
+    PrintTest(ls, X_test.astype(np.float32), y_test.values.astype(np.int32))
