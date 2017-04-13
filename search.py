8db32bede7b910c9b54ef9bbeed94f42fa0996a1
vladikM <mamaev.vlad1@gmail.com>
2017-04-13 20:48:14 +0300

    GBT gridsearch, gridsearch results to DataFrame

--- search.py
+++ search.py
@@ -125,22 +125,6 @@ def total_competition_score(estimator, X, y):
     return average_profit * deals
 
 
-def grid_scores_to_df(grid_scores):
-    """
-    Convert a sklearn.grid_search.GridSearchCV.grid_scores_ attribute to a tidy
-    pandas DataFrame where each row is a hyperparameter-fold combinatination.
-    """
-    rows = list()
-    for grid_score in grid_scores:
-        for fold, score in enumerate(grid_score.cv_validation_scores):
-            row = grid_score.parameters.copy()
-            row['fold'] = fold
-            row['score'] = score
-            rows.append(row)
-    df = pd.DataFrame(rows)
-    return df
-
-
 def grid_results_to_df(grid):
     results = pd.DataFrame.from_dict(grid.cv_results_)
     results = results.drop(
